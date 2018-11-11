957f2a7f0a3e20fd7787cec71a6d078207b7af0a
Jonathan <jonathanruedasmora@gmail.com>
2018-11-11 20:25:11 +0100

    Corrected some misconfiguration issues, deleted unused code and added forms and new views and urls

---
+++ wallafood/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from django.conf.urls import url
+from django.views.generic import RedirectView
+from . import views
+
+urlpatterns = [
+    path('', RedirectView.as_view(url='wallafood/login')),
+    path('login', views.index, name='login'),
+    path('register', views.register, name='register'),
+    path('doLogin', views.doLogin, name='doLogin'),
+    path('doLogout', views.doLogout, name='doLogout'),
+    path('forbidden', views.forbidden, name='forbidden'),
+    path('redirectForbidden', views.redirect_forbidden, name='redirect_forbidden'),
+    path('editProfile', views.edit_profile, name='edit_profile'),
+    path('deleteUser', views.delete_user, name='delete_user'),
+    path('changePassword', views.change_password, name='change_password'),
+    path('advertisements', views.advertisements, name='advertisements'),
+    path('advertisements/search', views.findAdvertisement, name='findAdvertisement')
+]
